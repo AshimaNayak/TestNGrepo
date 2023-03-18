0ea45a43fbfe74a340c1af9cda89afb87da08bef
Seema <skysthelimitqa.ashima@gmail.com>
2023-03-18 15:38:43 +0530

    initial version

---
+++ src/test/java/TestNG.java
@@ -0,0 +1,31 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG {
+    //WebDriver driver ;
+    @Test
+    public void f() {
+
+        String baseUrl = "https://www.toolsqa.com/";
+
+
+        System.out.println("Launching Google Chrome browser");
+        //driver = new ChromeDriver();
+        //driver.get(baseUrl);
+        String testTitle = "Free QA Automation Tools For Everyone";
+        //String originalTitle = driver.getTitle();
+       // Assert.assertEquals(originalTitle, testTitle);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Starting Test On Chrome Browser");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+       // driver.close();
+        System.out.println("Finished Test On Chrome Browser");
+    }
+}
